5ab741ca344c19e7e0a6810b9c0984baa009b419
Golsoum <145329299+Golsoum@users.noreply.github.com>
2024-10-04 10:02:36 +0200

    make an empty webpage

---
+++ cursusGeld/cursusGeld/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace cursusGeld
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
